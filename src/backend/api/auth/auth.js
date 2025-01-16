6434d95fe86b805f699e02e1506abf59c0aee80b
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2025-01-16 15:40:16 +0100

    feat : implémentation du service d'authentification

---
+++ src/backend/api/auth/auth.js
@@ -0,0 +1,11 @@
+class AuthService {
+constructor() {
+this.users = new Map();
+}
+async login(username, password) {
+// Logique d'authentification
+}
+async register(username, password) {
+// Logique d'enregistrement
+}
+}
